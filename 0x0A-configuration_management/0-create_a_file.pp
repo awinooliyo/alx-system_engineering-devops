21e473d74327753527b45cabc9352c667f6c83b4
Erick Awino <erick.otieno89@gmail.com>
2024-01-19 12:34:59 +0300

    Creating a file in Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Manifest file: create_school_file.pp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
