54ee5c253a60dcdcbe57a77aa58beb4f0be569d8
Erick Awino <erick.otieno89@gmail.com>
2024-01-25 08:13:47 +0300

    Task 5 advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,30 +7,31 @@ package { 'nginx':
   ensure => installed,
 }
 
-exec { "install nginx':
-  command => 'sudo apt -y update && sudo apt -y install nginx',
+exec { "install nginx":
+  command  => 'sudo apt -y update && sudo apt -y install nginx',
   provider => shell,
 }
 
-file { "/etc/nginx/sites-available/default':
+file { "/etc/nginx/sites-available/default":
   content => "server {
                 listen 80 default_server;
-                root/var/www/html;
+                root /var/www/html; # Added space here
                 location / {
-                index index.html;
+                  index index.html;
                 }
                 rewrite ^/redirect_me https://www.youtube.com permanent;
-        }",
+              }",
   require => Exec['install nginx'],
 }
 
-file { '/var/www/html/index.html':,
-  ensure => 'file',
+file { '/var/www/html/index.html':
+  ensure  => 'file',
   content => 'Hello World!',
   require => Exec['install nginx'],
 }
 
 exec { 'run':
-  command => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
+
