5cc2184d6cf323937ca485ac69ac76af328588e0
Erick Awino <erick.otieno89@gmail.com>
2024-01-31 08:37:00 +0300

    task 2 Advanced

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# Update package information
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y uodate',
+  path    => ['/usr/bin', '/bin],
+}
+
+# Install Nginx Package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create a basic html file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Configure Nginx to add the custom HTTP header
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-avalable/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
+}
