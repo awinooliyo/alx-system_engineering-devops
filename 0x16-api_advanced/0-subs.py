f928217f57666611b2e67f60f9f6293ad0f88a0c
Erick Awino <erick.otieno89@gmail.com>
2024-05-07 18:23:36 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,11 +20,11 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My User Agent 1.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get("data").get("subscribers")
+        data = response.json().get('data', {})
+        subscribers = data.get("subscribers", 0)
         return subscribers
     else:
         return 0
