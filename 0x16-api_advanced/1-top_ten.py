f518cfb82c93d0fb824a345263915a2c2967c7b0
Erick Awino <erick.otieno89@gmail.com>
2024-05-07 19:02:29 +0300

    Task 1 solution

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Querrying Reddit API
+A nodule consuming the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
 """
 import requests
 
@@ -10,23 +11,34 @@ def top_ten(subreddit):
     A function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit.
 
-    Args: A string representing the name of the subreddit.
+    Args:
+    subreddit (str): The name of the subreddit.
+    limit (int, optional): The number of posts to retrieve.
+                           Defaults to 10.
+    sort (str, optional): The sorting method for the posts.
+                          Defaults to 'top'.
 
     Returns:
-    - The titles of the to 10 hot posts listed for a given subreddit.
+    str: The titles of the to 10 hot posts listed for a given subreddit.
     - O or None if inbalid subreddit.
     """
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    base_url = 'https://www.reddit.com'
+    sort = 'top'
+    limit = 10
+    url = '{}/r/{}/.json?sort={}&limit={}'.format(
+        base_url, subreddit, sort, limit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
