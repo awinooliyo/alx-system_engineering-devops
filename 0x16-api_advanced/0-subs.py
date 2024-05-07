0ae030d4db23bc0f2925a5d163f6e3dfc4e509be
Erick Awino <erick.otieno89@gmail.com>
2024-05-07 16:52:15 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""Querrying the Reddit API"""
-
+"""
+Querrying the Reddit API
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Querry a subreddit and retrieve number of subscribers."""
+    """
+    A function that queries the Reddit API and retrieves
+    number of subscribers for a given subreddit
+
+    Args:
+    - subreddit: A string representing the name of the subreddit
+
+    Returns:
+    - An integer representing the number of subscribers
+    - 0 if the subreddit id invalid or inexistent
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"user-agent": "My user agent 1.0"}
+    headers = {"User-Agent": "My User Agent 1.0"}
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        sub_count = data.get("data").get("subscribers")
-        return sub_count
+        subscribers = data.get("data").get("subscribers")
+        return subscribers
     else:
         return 0
